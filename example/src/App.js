8c59a7c06f464af2c0041e72d89d3f62861f15d9
Hobin Ryu <hobincar@kaist.ac.kr>
2018-05-07 20:09:09 +0900

    react-clustergrammer 1.0.0

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react'
-
-import ExampleComponent from 'react-clustergrammer'
+import React, { Component } from 'react';
+import Clustergrammer from 'react-clustergrammer';
 
 export default class App extends Component {
   render () {
+    const nRow = 100;
+    const nCol = 100;
+    const nCluster = 10;
+    const nLink = 10000;
+
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
-      </div>
+      <Clustergrammer
+        network_data={{
+          'row_nodes': Array(nRow).fill(0).map((_, i) => ({
+            name: `ROW-${i}`,
+            clust: parseInt(Math.random()*nCluster),
+            rank: parseInt(Math.random()*10),
+            rankvar: parseInt(Math.random()*10),
+            group: [],
+          })),
+          'col_nodes': Array(nCol).fill(0).map((_, i) => ({
+            name: `COL-${i}`,
+            clust: parseInt(Math.random()*nCluster),
+            rank: parseInt(Math.random()*10),
+            rankvar: parseInt(Math.random()*10),
+            group: [],
+          })),
+          'links': Array(nLink).fill(0).map(_ => ({
+            source: parseInt(Math.random()*nRow),
+            target: parseInt(Math.random()*nCol),
+            value: Math.random() * 2 - 1,
+          })),
+        }}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
     )
   }
 }
